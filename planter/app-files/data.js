893b8b59052f41421452ae9b0bef97387ab0dd85
philippjiuweima <p_ma@web.de>
2021-07-20 21:01:46 +0200

    1

---
+++ planter/app-files/data.js
@@ -0,0 +1,253 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-ansicht-vorne",
+      "name": "Ansicht Vorne",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 0.025351336043542005,
+        "pitch": 0.3180705288505816,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.772003237076996,
+          "pitch": 0.6910525204252345,
+          "rotation": 0,
+          "target": "3-hof-vorne"
+        },
+        {
+          "yaw": -0.29995720020157,
+          "pitch": -0.07156446762907187,
+          "rotation": 0,
+          "target": "4-hof-hinten"
+        },
+        {
+          "yaw": 0.5934525012298071,
+          "pitch": 0.13848251919142918,
+          "rotation": 0,
+          "target": "5-chicken-pov"
+        },
+        {
+          "yaw": 0.3281548089975921,
+          "pitch": -0.07389536210581582,
+          "rotation": 0,
+          "target": "2-dachterasse"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.9194208539867716,
+          "pitch": -0.1791304888199612,
+          "title": "Planter",
+          "text": "Lebensmittelproduktion durch die moderne Landwirtschaft, ist eine der treibenden Kräfte bei der sich anbahnenden Klimakrise. Etwa ein Drittel der globalen Treibhausgas Emissionen stammen aus der Landwirtschaft und Tierhaltung. Den größten Einfluss, auf diese Emissionen, haben wir durch unser Konsumverhalten. Um der Entkopplung des Menschen von der Lebensmittelproduktion entgegenzuwirken, muss Agrarproduktion an den Ort des Verbrauchers. Hierzu wird ein gemischtes Wohn und Arbeitsgebäude für die zur IBA 27 entstehende MakerCity vorgeschlagen. In dem Gebäude werden gemeinschaftliches Wohnen und Arbeiten kombiniert mit kollektiver Lebensmittelproduktion in einem sich über das Gebäude ziehenden Permakultur Garten."
+        }
+      ]
+    },
+    {
+      "id": "1-brcke",
+      "name": "Brücke",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": -2.507782795067506,
+        "pitch": 0.23024513760636722,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-dachterasse",
+      "name": "Dachterasse",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": -2.287596703875245,
+        "pitch": 0.3160110100846296,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-hof-vorne",
+      "name": "Hof Vorne",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 2.217700827598712,
+        "pitch": -0.005290168751447055,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-hof-hinten",
+      "name": "Hof Hinten",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 2.9947090651446686,
+        "pitch": 0.5356713508876254,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-chicken-pov",
+      "name": "Chicken POV",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 3.0615618011043857,
+        "pitch": 0.1493066933824423,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Planter",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
